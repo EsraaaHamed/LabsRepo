6eacb19bdea29f6aa4b1bfffe661497d659ea3c4
EsraaaHamed <47310306+EsraaaHamed@users.noreply.github.com>
2019-05-09 14:51:14 +0200

    login static table

---
+++ loginWithResponsive/loginWithResponsive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loginWithResponsive
+//
+//  Created by JETS Mobile Lab-7 on 5/9/19.
+//  Copyright © 2019 iti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
